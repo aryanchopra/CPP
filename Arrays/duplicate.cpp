0a790c1d64b76b8770c6ef1da5023bbfc67ab357
aryanchopra <chopraryan@gmail.com>
2020-12-19 18:24:15 +0530

    more problems

---
+++ Arrays/duplicate.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void duplicate(int arr[],int n){
+    for(int i=0;i<n;i++){
+        arr[arr[i]%n]= arr[arr[i]%n]+n;
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]/n>1){
+            cout<<i<<" ";
+        }
+    }
+
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    duplicate(arr,n);
+}
